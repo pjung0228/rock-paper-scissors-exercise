28619810e511e78430b5804dcd5db78ced71871f
pjung0228 <108762047+pjung0228@users.noreply.github.com>
2022-07-13 14:58:03 -0700

    Create game.py

---
+++ game.py
@@ -0,0 +1,64 @@
+
+import random
+
+
+print ("Rock, Paper, Scissors, Shoot!")
+
+# USER INPUTS
+user_choice = input("Please make a selection ('rock', 'paper', 'scissors'):")
+print (user_choice)
+
+
+# VALIDATE USER INPUTS
+user_choice = input("Please make a selection ('rock','paper','scissors'):")
+
+print("You chose:", user_choice)
+print(f"You chose: '{user_choice}'")
+
+# COMPUTER CHOICE
+
+
+#IMPORT RANDOM
+valid_options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(valid_options)
+print("computer chose:", computer_choice)
+
+# DETERMINE THE WINNER
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# DISPLAY RESULTS
+# -------------------
+# Welcome 'Player One' to my Rock-Paper-Scissors game...
+# -------------------
+# Please choose either 'rock', 'paper', or 'scissors': rock
+# You chose: 'rock'
+# The computer chose: 'paper'
+# -------------------
+# Oh, the computer won. It's ok.
+# -------------------
+# Thanks for playing. Please play again!
+
+
+
+
+
+
+
+
+
+
+
+
+
